470df6af6efcd70946bf54f0a168f40fda011ebe
Erick Zhao <erick.zhao@mail.mcgill.ca>
2019-03-07 00:48:09 -0500

    🚧 Add basic boilerplate for source plugin

---
+++ gatsby-node.js
@@ -0,0 +1,11 @@
+
+
+exports.sourceNodes = (
+  { actions, createNodeId, createContentDigest },
+  configOptions
+) => {
+  const { createNode } = actions
+
+  // plugin code goes here...
+  console.log("Testing my plugin", configOptions)
+}
