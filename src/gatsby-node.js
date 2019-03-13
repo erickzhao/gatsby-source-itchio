cdbb63ae71266dcdecd79d3e00aed1e68ce88df1
Erick Zhao <erick.zhao@mail.mcgill.ca>
2019-03-12 23:38:23 -0400

    ✨ Add transpiling w/ Babel

---
+++ src/gatsby-node.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+exports.sourceNodes = async ({ actions, createNodeId, createContentDigest }, configOptions) => {
+  const { createNode } = actions;
+
+  const { key } = configOptions;
+  const apiUrl = `https://itch.io/api/1/${key}/my-games`;
+  const { data } = await axios.get(apiUrl);
+  const { games } = data;
+
+  const gameNodes = games.map((game) => {
+    const nodeId = createNodeId(`itchio-game-${game.id}`);
+    const nodeContent = JSON.stringify(game);
+    const nodeData = {
+      ...game,
+      id: nodeId,
+      parent: null,
+      children: [],
+      internal: {
+        type: 'ItchioGame',
+        content: nodeContent,
+        contentDigest: createContentDigest(game),
+      },
+    };
+
+    return nodeData;
+  });
+
+  gameNodes.forEach(node => createNode(node));
+};
